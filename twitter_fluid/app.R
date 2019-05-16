3e28125ff4a204806bc21c7164ca0c4946dac5ea
Will Bonnell <wtb3na@virginia.edu>
2019-05-16 15:27:46 -0500

    adding full ui into tabset panel

--- twitter_fluid/app.R
+++ twitter_fluid/app.R
@@ -6,51 +6,71 @@ ui <- fluidPage(
   
   headerPanel("Twitter rules drafter"),
   
-  fluidRow(
+  tabsetPanel(
     
-    column(3, 
-           shinyjs::useShinyjs(),
-           br(),
-           id = "side-panel",
-           textInput("mytext", "Enter a search term", placeholder = "votecast"),
-           actionButton("add" ,"Add")),
-    column(8,
-           h4("Terms"),
-           verbatimTextOutput("text_term")
-           )
-    
-    
-  ),
-  
-  fluidRow(
-    
-    column(3, 
-           shinyjs::useShinyjs(),
-           br(),
-           id = "side-panel2",
-           textInput("mytext2", "Enter a set of URL keywords", placeholder = "stellar debut votecast"),
-           actionButton("add2" ,"Add")),
-    column(8,
-           h4("URL terms"),
-           verbatimTextOutput("text_urlterm"))
-    
-  ),
-  
-  fluidRow(
-    
-    column(3, 
-           shinyjs::useShinyjs(),
-           br(),
-           id = "side-panel3",
-           textInput("mytext3", "Enter a URL", placeholder = "apnorc.org"),
-           actionButton("add3" ,"Add")),
-    column(8,
-           h4("URLs"),
-           verbatimTextOutput("text_url"))
-    
+    tabPanel("Rules",
+             
+             fluidRow(
+               
+               column(3, 
+                      shinyjs::useShinyjs(),
+                      br(),
+                      id = "side-panel",
+                      textInput("mytext", "Enter a search term", placeholder = "votecast"),
+                      actionButton("add" ,"Add")),
+               column(8,
+                      h4("Terms"),
+                      verbatimTextOutput("text_term")
+               )
+               
+               
+             ),
+             
+             fluidRow(
+               
+               column(3, 
+                      shinyjs::useShinyjs(),
+                      br(),
+                      id = "side-panel2",
+                      textInput("mytext2", "Enter a set of URL keywords", placeholder = "stellar debut votecast"),
+                      actionButton("add2" ,"Add")),
+               column(8,
+                      h4("URL terms"),
+                      verbatimTextOutput("text_urlterm"))
+               
+             ),
+             
+             fluidRow(
+               
+               column(3, 
+                      shinyjs::useShinyjs(),
+                      br(),
+                      id = "side-panel3",
+                      textInput("mytext3", "Enter a URL", placeholder = "apnorc.org"),
+                      actionButton("add3" ,"Add")),
+               column(8,
+                      h4("URLs"),
+                      verbatimTextOutput("text_url"))
+               
+               
+             ),
+             
+             fluidRow(
+               column(11,
+                      br(),
+                      br(),
+                      h3("Twitter Syntax")
+                      
+               )
+             )  
+    ),
+    tabPanel("Explanation",
+             br(),
+             "This app exists for the purpose of generating JSON syntax for search terms for the twitter firehose API. You simply add in search terms related to the release, add the phrases associated with URLs, and then finally add relevant URLs. The app will output syntax associated with the terms displayed in the right hand column."
+             
+    )
     
   )
-  
 )
 
 server <- function(input, output, session) {
