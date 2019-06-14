154ea8d5461558e95870860c0e14ed47609ed631
Will Bonnell <wtb3na@virginia.edu>
2019-06-14 12:22:47 -0500

    adding script of examples of the internal data manipulation

--- test_work.R
+++ test_work.R
@@ -12,4 +12,31 @@ test3 <- as.vector(test2[[1]]) %>% trimws(which = "both")
 test4 <- sapply(test3, function(x) ifelse(str_detect(x, "[:blank:]"), paste("\\\"", x, "\\\"", sep = ""), paste(x)))
 
 
-paste(test2, sep = "", collapse)
+paste("(", paste(test4, sep = " ", collapse = " OR "), ")", sep = "")
+
+
+urlterms <- "here we really fucking go, here another set"
+
+terms <- strsplit(urlterms, ",") 
+
+terms2 <- as.vector(terms[[1]]) %>% trimws(which = "both") %>%
+  strsplit(" ")
+
+terms3 <- sapply(terms2, function(x) paste("url_contains:", x, "OR", collapse = " "))
+
+terms4 <- sapply(terms3, function(x) str_sub(x, 1, str_length(x)-3))
+
+terms5 <- sapply(terms4, function(x) paste("(", x, ")", sep = ""))
+
+
+url1 <- "apnorc.org/blahblah, apnews.com/blahblah"
+
+url2 <- strsplit(url1, ",")
+
+url3 <- as.vector(url2[[1]]) %>% trimws(which = "both")
+
+url4 <- test4 <- sapply(url3, function(x) paste("url_contains:", "\\\"", x, "\\\"", sep = ""))
+
+url5 <- paste(url4, sep = " ", collapse = " OR ")
+
+              
